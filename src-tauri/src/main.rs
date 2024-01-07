3de837eac2b2b301b11dbcced63b95d9ac2b29e0
masayuki-kono@mamezou.com <masayuki-kono@gmail.com>
2024-01-08 04:59:42 +0900

    add subscriber

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use futures::stream::StreamExt;
 use std::sync::{Arc, Mutex};
+use tauri::Manager;
 
 struct Publishers {
     operation1: r2r::Publisher<r2r::std_msgs::msg::String>,
@@ -24,7 +26,8 @@ fn button2_pushed(message: &str, publishers: tauri::State<Arc<Mutex<Publishers>>
     publishers.lock().unwrap().operation2.publish(&msg).unwrap();
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let ctx = r2r::Context::create().unwrap();
     let mut node = r2r::Node::create(ctx, "web_ui", "").unwrap();
 
@@ -43,6 +46,11 @@ fn main() {
             .unwrap(),
     }));
 
+    let sub_operation_enabled = node
+        .subscribe::<r2r::std_msgs::msg::Bool>("/operation_enabled", r2r::QosProfile::default())
+        .unwrap();
+    let operation_enabled = Arc::new(Mutex::new(false));
+
     std::thread::spawn(move || loop {
         node.spin_once(std::time::Duration::from_millis(100));
     });
@@ -50,6 +58,26 @@ fn main() {
     tauri::Builder::default()
         .manage(publishers)
         .invoke_handler(tauri::generate_handler![button1_pushed, button2_pushed])
+        .setup(move |app| {
+            let app_handle = app.handle();
+
+            tokio::spawn(async move {
+                sub_operation_enabled
+                    .for_each(|msg| {
+                        let mut enabled = operation_enabled.lock().unwrap();
+                        *enabled = msg.data;
+                        let payload = msg.data;
+                        let main_window = app_handle.get_window("main").unwrap();
+                        main_window
+                            .emit("operation-enabled-updated", &payload)
+                            .unwrap();
+                        futures::future::ready(())
+                    })
+                    .await
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { listen } from "@tauri-apps/api/event";
 
 function App() {
   const [message, setMessage] = useState("");
+  const [operationEnabled, setOperationEnabled] = useState(true);
+
+  const disabledButtonStyle = {
+    backgroundColor: "#A9A9A9",
+    cursor: "not-allowed",
+  };
 
   async function button1_pushed() {
     await invoke("button1_pushed", { message: message });
@@ -13,6 +20,20 @@ function App() {
     await invoke("button2_pushed", { message: message });
   }
 
+  useEffect(() => {
+    const unlistenPromise = listen<boolean>(
+      "operation-enabled-updated",
+      (event) => {
+        setOperationEnabled(event.payload);
+      }
+    );
+    return () => {
+      void unlistenPromise.then((unlistenFn) => {
+        unlistenFn();
+      });
+    };
+  }, []);
+
   return (
     <div className="container">
       <form className="row">
@@ -21,8 +42,20 @@ function App() {
           onChange={(e) => setMessage(e.currentTarget.value)}
           placeholder="Enter a message..."
         />
-        <button onClick={() => button1_pushed()}>Publish1</button>
-        <button onClick={() => button2_pushed()}>Publish2</button>
+        <button
+          style={!operationEnabled ? disabledButtonStyle : {}}
+          disabled={!operationEnabled}
+          onClick={() => button1_pushed()}
+        >
+          Publish1
+        </button>
+        <button
+          style={!operationEnabled ? disabledButtonStyle : {}}
+          disabled={!operationEnabled}
+          onClick={() => button2_pushed()}
+        >
+          Publish2
+        </button>
       </form>
     </div>
   );
