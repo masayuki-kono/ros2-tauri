112cbe3438aa0550ed6d030d3ff82f1c37f1aa53
masayuki-kono@mamezou.com <masayuki-kono@gmail.com>
2024-01-08 02:32:23 +0900

    impl publisher

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,21 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use std::sync::{Arc, Mutex};
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn button_pushed(
+    message: &str,
+    publisher: tauri::State<Arc<Mutex<r2r::Publisher<r2r::std_msgs::msg::String>>>>,
+) {
+    let msg = r2r::std_msgs::msg::String {
+        data: message.to_string(),
+    };
+    publisher.lock().unwrap().publish(&msg).unwrap();
 }
 
 fn main() {
     let ctx = r2r::Context::create().unwrap();
     let mut node = r2r::Node::create(ctx, "web_ui", "").unwrap();
+
+    let pub_operation = Arc::new(Mutex::new(
+        node.create_publisher::<r2r::std_msgs::msg::String>(
+            "/operation",
+            r2r::QosProfile::default(),
+        )
+        .unwrap(),
+    ));
+
     std::thread::spawn(move || loop {
         node.spin_once(std::time::Duration::from_millis(100));
     });
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(pub_operation)
+        .invoke_handler(tauri::generate_handler![button_pushed])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  async function publish() {
+    await invoke("button_pushed", { message: message });
   }
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
       <form
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          publish();
         }}
       >
         <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          id="message-input"
+          onChange={(e) => setMessage(e.currentTarget.value)}
+          placeholder="Enter a message..."
         />
-        <button type="submit">Greet</button>
+        <button type="submit">Publish</button>
       </form>
-
-      <p>{greetMsg}</p>
     </div>
   );
 }
