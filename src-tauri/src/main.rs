d571613f6e87969d8e774fdb21d4e8c437b26664
masayuki-kono@mamezou.com <masayuki-kono@gmail.com>
2024-01-08 03:16:16 +0900

    add publisher

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,36 +3,53 @@
 
 use std::sync::{Arc, Mutex};
 
+struct Publishers {
+    operation1: r2r::Publisher<r2r::std_msgs::msg::String>,
+    operation2: r2r::Publisher<r2r::std_msgs::msg::String>,
+}
+
 #[tauri::command]
-fn button_pushed(
-    message: &str,
-    publisher: tauri::State<Arc<Mutex<r2r::Publisher<r2r::std_msgs::msg::String>>>>,
-) {
+fn button1_pushed(message: &str, publishers: tauri::State<Arc<Mutex<Publishers>>>) {
     let msg = r2r::std_msgs::msg::String {
         data: message.to_string(),
     };
-    publisher.lock().unwrap().publish(&msg).unwrap();
+    publishers.lock().unwrap().operation1.publish(&msg).unwrap();
+}
+
+#[tauri::command]
+fn button2_pushed(message: &str, publishers: tauri::State<Arc<Mutex<Publishers>>>) {
+    let msg = r2r::std_msgs::msg::String {
+        data: message.to_string(),
+    };
+    publishers.lock().unwrap().operation2.publish(&msg).unwrap();
 }
 
 fn main() {
     let ctx = r2r::Context::create().unwrap();
     let mut node = r2r::Node::create(ctx, "web_ui", "").unwrap();
 
-    let pub_operation = Arc::new(Mutex::new(
-        node.create_publisher::<r2r::std_msgs::msg::String>(
-            "/operation",
-            r2r::QosProfile::default(),
-        )
-        .unwrap(),
-    ));
+    let publishers = Arc::new(Mutex::new(Publishers {
+        operation1: node
+            .create_publisher::<r2r::std_msgs::msg::String>(
+                "/operation1",
+                r2r::QosProfile::default(),
+            )
+            .unwrap(),
+        operation2: node
+            .create_publisher::<r2r::std_msgs::msg::String>(
+                "/operation2",
+                r2r::QosProfile::default(),
+            )
+            .unwrap(),
+    }));
 
     std::thread::spawn(move || loop {
         node.spin_once(std::time::Duration::from_millis(100));
     });
 
     tauri::Builder::default()
-        .manage(pub_operation)
-        .invoke_handler(tauri::generate_handler![button_pushed])
+        .manage(publishers)
+        .invoke_handler(tauri::generate_handler![button1_pushed, button2_pushed])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,24 @@ import "./App.css";
 function App() {
   const [message, setMessage] = useState("");
 
-  async function publish() {
-    await invoke("button_pushed", { message: message });
+  async function button1_pushed() {
+    await invoke("button1_pushed", { message: message });
+  }
+
+  async function button2_pushed() {
+    await invoke("button2_pushed", { message: message });
   }
 
   return (
     <div className="container">
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          publish();
-        }}
-      >
+      <form className="row">
         <input
           id="message-input"
           onChange={(e) => setMessage(e.currentTarget.value)}
           placeholder="Enter a message..."
         />
-        <button type="submit">Publish</button>
+        <button onClick={() => button1_pushed()}>Publish1</button>
+        <button onClick={() => button2_pushed()}>Publish2</button>
       </form>
     </div>
   );
